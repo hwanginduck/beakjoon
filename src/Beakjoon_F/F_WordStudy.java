fbad62fb81859e2e8f27523c4e06fcc115569287
Kaka <kaka7939@kakao.com>
2022-08-05 23:51:29 +0900

    update F_WordStudy

--- src/Beakjoon_F/F_WordStudy.java
+++ src/Beakjoon_F/F_WordStudy.java
@@ -1,10 +1,26 @@
 package Beakjoon_F;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class F_WordStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			String word = br.readLine();
+			
+			
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 
 }
