fbc2baa50cc231c31948cbee149dc72ff8747528
Kaka <kaka7939@kakao.com>
2022-08-07 20:04:31 +0900

    update F_WordStudy

--- src/Beakjoon_F/F_WordStudy.java
+++ src/Beakjoon_F/F_WordStudy.java
@@ -9,25 +9,40 @@ public class F_WordStudy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		try {
+//			문자를 buffer로 입력 받음
 			String word = br.readLine();
-			
-			
-			
-			for(int i=0; i<word.length(); i++)
-			{
-				char a = word.charAt(i);
-				int ask = a;
+			word = word.toUpperCase();
+			int alpha[] = new int[word.length()];
+
+//			String > 한글자씩 나눠주고 > 대문자로 바꿔주 > 한글자씩을 char형으로 변환 > int 형으로 변환
+			char[] words = word.toCharArray();
+			for(int i=0; i<words.length; i++){
+				alpha[i] = words[i]-65;	
 			}	
+//			알파벳에 맞게 카운팅을 해줌
+			int count[] = new int[26];
+//			
+			for(int i=0; i<alpha.length; i++) {
+				count[alpha[i]]++;
+			}
 			
+			int MaxCount = 0; //많이 나온 횟수
+			int ManyAlpha = 0; //어떤 알파벳이 많이 나왓는지 확인
 			
+			for(int i=0; i<count.length; i++) {
+				if(MaxCount < count[i]) {
+					MaxCount = count[i];
+					ManyAlpha = i;
+				}		
+			}
+			ManyAlpha = ManyAlpha+65;
+			char printAlpha = (char)ManyAlpha;
+			String printprint = String.valueOf(printAlpha);
+			
+			System.out.println("최다 출현 알파벳은 " +printprint +"최다 횟수는 " +MaxCount);
 		}catch(IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
 	}
-
 }
