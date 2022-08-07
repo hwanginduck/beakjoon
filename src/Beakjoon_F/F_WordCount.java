c4b946549ce4e49cde8b2686567c05c7b1fcf892
Kaka <kaka7939@kakao.com>
2022-08-08 00:56:46 +0900

    update F_WordCount

--- src/Beakjoon_F/F_WordCount.java
+++ src/Beakjoon_F/F_WordCount.java
@@ -1,26 +1,23 @@
 package Beakjoon_F;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class F_WordCount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
 		
-		try {
-			String input = br.readLine();
+			String input = sc.nextLine();
 			input = input.trim();
+			
 			String count[] = input.split(" ");
 			
-			System.out.println(count.length);				
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			if(count[0].equals("")) {
+				System.out.println(count.length -1);
+			}else {
+				System.out.println(count.length);				
+			}
 	}
 }
